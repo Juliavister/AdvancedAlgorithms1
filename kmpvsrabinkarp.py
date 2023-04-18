96bdfed97d4bf412ba53a29d26eb8caa9ed7de61
Doğa Kurtoğlu <71259401+ehady@users.noreply.github.com>
2023-04-18 17:54:44 +0200

    Update kmpvsrabinkarp.py

--- kmpvsrabinkarp.py
+++ kmpvsrabinkarp.py
@@ -1,27 +1,64 @@
 import timeit
 
-def kmp(pattern, text):
-    m = len(pattern)
-    n = len(text)
-    pi = [0] * m
-    j = 0
-    for i in range(1, m):
-        while j > 0 and pattern[j] != pattern[i]:
-            j = pi[j - 1]
-        if pattern[j] == pattern[i]:
+def kmp(pat, txt):
+    M = len(pat)
+    N = len(txt)
+
+    # create lps[] that will hold the longest prefix suffix
+    # values for pattern
+    lps = [0] * M
+    j = 0  # index for pat[]
+
+    # Preprocess the pattern (calculate lps[] array)
+    computeLPSArray(pat, M, lps)
+
+    i = 0  # index for txt[]
+    indices = []  # initialize list of indices
+    while i < N:
+        if pat[j] == txt[i]:
+            i += 1
             j += 1
-        pi[i] = j
-    j = 0
-    for i in range(n):
-        while j > 0 and pattern[j] != text[i]:
-            j = pi[j - 1]
-        if pattern[j] == text[i]:
-            j += 1
-        if j == m:
-            # pattern found at position i - m + 1
-            return i - m + 1
-    # pattern not found
-    return -1
+
+        if j == M:
+            indices.append(i - j)  # pattern found, add index to list
+            j = lps[j - 1]
+
+        # mismatch after j matches
+        elif i < N and pat[j] != txt[i]:
+            # Do not match lps[0..lps[j-1]] characters,
+            # they will match anyway
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    return indices  # return list of indices where pattern is found
+
+
+def computeLPSArray(pat, M, lps):
+    len = 0  # length of the previous longest prefix suffix
+
+    lps[0] = 0
+    i = 1
+
+    # the loop calculates lps[i] for i = 1 to M-1
+    while i < M:
+        if pat[i] == pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+        else:
+            # This is tricky. Consider the example.
+            # AAACAAAA and i = 7. The idea is similar
+            # to search step.
+            if len != 0:
+                len = lps[len - 1]
+
+                # Also, note that we do not increment i here
+            else:
+                lps[i] = 0
+                i += 1
+
 
 def rabin_karp(pattern, text):
     pattern_hash = hash(pattern)
