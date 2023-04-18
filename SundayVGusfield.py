43cab585529a7b44e6e73667a821e55f12e2a138
Doğa Kurtoğlu <71259401+ehady@users.noreply.github.com>
2023-04-18 17:53:48 +0200

    Update SundayVGusfield.py

--- SundayVGusfield.py
+++ SundayVGusfield.py
@@ -1,59 +1,81 @@
 import timeit
+import random
+import string
 
 def binary_sunday(pattern, text):
+    matches = []
     m = len(pattern)
     n = len(text)
-    skip = [m] * 256
+    skip_table = {}
     for i in range(m):
-        skip[ord(pattern[i])] = m - i - 1
+        skip_table[pattern[i]] = m - i
     i = 0
     while i <= n - m:
-        j = m - 1
-        while j >= 0 and text[i + j] == pattern[j]:
-            j -= 1
-        if j < 0:
-            # pattern found at position i
-            return i
-        i += skip[ord(text[i + m])] if i + m < n else 1
-    # pattern not found
-    return -1
+        # check if the current character is a potential match
+        if text[i] in skip_table:
+            j = 0
+            while j < m and (text[i + j] == pattern[j]):
+                j += 1
+            if j == m:
+                matches.append(i)
+            i += skip_table[text[i]]
+        else:
+            i += m + 1
+    return matches
 
 def gusfield_z(pattern, text):
-    m = len(pattern)
-    n = len(text)
-    z = [0] * n
+    """
+       Gusfield's Z algorithm for pattern matching.
+       """
+    concat = pattern + "$" + text
+    z = [0] * len(concat)
     l, r = 0, 0
-    for i in range(1, n):
+    for i in range(1, len(concat)):
         if i > r:
-            j = 0
-            while i + j < n and text[i + j] == text[j]:
-                j += 1
-            if j > 0:
-                z[i] = j
-                l, r = i, i + j - 1
+            l, r = i, i
+            while r < len(concat) and concat[r - l] == concat[r]:
+                r += 1
+            z[i] = r - l
+            r -= 1
         else:
             k = i - l
             if z[k] < r - i + 1:
                 z[i] = z[k]
             else:
-                j = r - i + 1
-                while r + j < n and text[r + j] == text[r - i + 1 + j]:
-                    j += 1
-                z[i] = r - i + 1 + j
-                l, r = i, r + j - 1
-        if z[i] == m:
-            # pattern found at position i - m - 1
-            return i - m - 1
-    # pattern not found
+                l = i
+                while r < len(concat) and concat[r - l] == concat[r]:
+                    r += 1
+                z[i] = r - l
+                r -= 1
+    matches = []
+    for i in range(len(z)):
+        if z[i] == len(pattern):
+            return i - len(pattern) - 1
     return -1
 
+# specify a pattern of at least 10 characters
+pattern = "GTCTCGCGCG"
+
+# specify a long text of at least 100kB
+text = "A" * 100000
+
+# test Binary Sunday
+binary_sunday_time = timeit.timeit(lambda: binary_sunday(pattern, text), number=10)
+print("Binary Sunday time:", binary_sunday_time/10)
+
+# test Gusfield Z
+gusfield_z_time = timeit.timeit(lambda: gusfield_z(pattern, text), number=10)
+print("Gusfield Z time:", gusfield_z_time/10)
+
+
 # test with a repeating pattern and text
-pattern = "ababababab"
-text = pattern * ( 1024 * 1024 // len(pattern))  # 1MB text
-binary_sunday_time = timeit.timeit(lambda: binary_sunday(pattern, text), number=100)
-print("Binary Sunday time:", binary_sunday_time)
+#pattern = "a" * 1000
+#text = "a" * (100 * 1024)
+
+#binary_sunday_time = timeit.timeit(lambda: binary_sunday(pattern, text), number=1)
+#print("Binary Sunday time:", binary_sunday_time)
 
 
 # Time Gusfield Z algorithm
-gusfield_z_time = timeit.timeit(lambda: gusfield_z(pattern, text), number=100)
-print("Gusfield Z time:", gusfield_z_time)
+#gusfield_z_time = timeit.timeit(lambda: gusfield_z(pattern, text), number=1)
+#print("Gusfield Z time:", gusfield_z_time)
