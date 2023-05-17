0176592f37e072a3b7e2262f6acc73d55f45a5c1
Doğa Kurtoğlu <71259401+ehady@users.noreply.github.com>
2023-05-17 14:51:47 +0200

    Update GusfieldZ.py

--- GusfieldZ.py
+++ GusfieldZ.py
@@ -2,31 +2,31 @@ def gusfield_z(pattern, text):
     """
     Gusfield's Z algorithm for pattern matching.
     """
-    concat = pattern + "$" + text
+    concat = pattern + "$" + text # Concatenating them helps simplify the algorithm's logic and allows us to find matches efficiently using the Z-array.
     z = [0] * len(concat)
     l, r = 0, 0
-    for i in range(1, len(concat)):
-        if i > r:
-            l, r = i, i
-            while r < len(concat) and concat[r - l] == concat[r]:
-                r += 1
-            z[i] = r - l
+    for i in range(1, len(concat)): # iterate through concat start for index 1 bc index 0 is always 0
+        if i > r: # if true, means we dont have info abt the lcp(longest common prefix) beyond this point so we need to compare the pattern with the suffix to fing lcp
+            l, r = i, i #set a new Z-box with the values
+            while r < len(concat) and concat[r - l] == concat[r]: #until a mismatch is found or concat reaches the end
+                r += 1 #expand the Z-box 
+            z[i] = r - l #the Z value at the current position is set as the length of the matching prefix, which is r - l 
             r -= 1
-        else:
-            k = i - l
-            if z[k] < r - i + 1:
-                z[i] = z[k]
-            else:
+        else: # we have info about available from previous calculations
+            k = i - l #relative position within the z box
+            if z[k] < r - i + 1: # if the k value is less than the remaining length
+                z[i] = z[k] # copy the value
+            else: #otherwise,
                 l = i
-                while r < len(concat) and concat[r - l] == concat[r]:
-                    r += 1
-                z[i] = r - l
+                while r < len(concat) and concat[r - l] == concat[r]: # comparing characters from the rightmost position (r) until a mismatch is found or the concat reaches the end
+                    r += 1 # expand the Z-box
+                z[i] = r - l # The Z-value at the current position is set as the length of the new matching prefix, which is (r - l).
                 r -= 1
-    matches = []
-    for i in range(len(z)):
+    matches = [] # After calculating the Z-values for all positions, we iterate through the Z-array to find matches.
+    for i in range(len(z)):  # If a Z-value is equal to the length of the pattern, it means we have found a match.
         if z[i] == len(pattern):
             return i - len(pattern) - 1
-    return -1
+    return -1 # no match is found
 
 import docx
 import time
