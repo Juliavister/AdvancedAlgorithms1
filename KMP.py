452f94d6e3be18d0ccc28b9850a5c7e4994c4020
Doğa Kurtoğlu <71259401+ehady@users.noreply.github.com>
2023-05-17 15:19:42 +0200

    Update KMP.py

--- KMP.py
+++ KMP.py
@@ -13,21 +13,21 @@ def KMPSearch(pat, txt):
     j = 0 # index for pat[]
 
     # Preprocess the pattern (calculate lps[] array)
-    computeLPSArray(pat, M, lps)
+    computeLPSArray(pat, M, lps) # calculate longest prefix suffix values are stored in lps
 
     i = 0 # index for txt[]
     indices = [] # initialize list of indices
     while i < N:
         if pat[j] == txt[i]:
-            i += 1
+            i += 1 # proceed to the next characters 
             j += 1
 
-        if j == M:
+        if j == M: #complete match is found 
             indices.append(i-j) # pattern found, add index to list
             j = lps[j-1]
 
         # mismatch after j matches
-        elif i < N and pat[j] != txt[i]:
+        elif i < N and pat[j] != txt[i]: #avoid unnecesary comparisons by skipping ahead
             # Do not match lps[0..lps[j-1]] characters,
             # they will match anyway
             if j != 0:
@@ -44,15 +44,15 @@ def computeLPSArray(pat, M, lps):
     len = 0 # length of the previous longest prefix suffix
 
     lps[0] = 0
-    i = 1
+    i = 1 #second index of the pattern, first index doesnT have any proper suffixes
 
     # the loop calculates lps[i] for i = 1 to M-1
     while i < M:
-        if pat[i] == pat[len]:
-            len += 1
+        if pat[i] == pat[len]: #means we found a longer proper suffix, meaning the suffix that is not equal to the entire string itself
+            len += 1 # upgrade the length of the suffix 
             lps[i] = len
-            i += 1
-        else:
+            i += 1 # next position
+        else: #current suffix is not a proper suffix anymore
             # This is tricky. Consider the example.
             # AAACAAAA and i = 7. The idea is similar
             # to search step.
