8ccf73f1d5e35e98588f4f9e39dbc01605f7ba71
Mikołaj Pieńkosz <73973110+mikolajp2137@users.noreply.github.com>
2023-05-28 16:13:12 +0200

    Update main.py

--- photoKarp/main.py
+++ photoKarp/main.py
@@ -1,20 +1,20 @@
 import numpy as np
-from rabin_karp_image_search import rabin_karp_image_search
+from rabin_karp_image_search import rabin_karp_image_search as rabin_karp
 
-image = np.array([[1, 2, 3, 4],
-                  [5, 6, 7, 8],
-                  [9, 1, 2, 3],
-                  [4, 5, 6, 7]])
-pattern = np.array([[2, 3],
-                    [6, 7]])
-location = rabin_karp_image_search(image, pattern)
+image = np.array([['1', '2', '3', '4'],
+                  ['5', '6', '7', '8'],
+                  ['9', '1', '2', '3'],
+                  ['4', '5', '6', '7']])
+pattern = np.array([['2', '3'],
+                    ['6', '7']])
+location = rabin_karp(image, pattern)
 print(location)
 
 
 image2 = np.array([[1,'b','c','d'],
-                   ['a',1,'e','f'],
+                   ['1',1,'e','f'],
                    ['z','z','z','z']])
-pattern2 = np.array([['a','1'],
-                     ['h','z']])
-location2 = rabin_karp_image_search(image2,pattern2)
+pattern2 = np.array([[1,1],
+                     ['z','z']])
+location2 = rabin_karp(image2,pattern2)
 print(location2)
